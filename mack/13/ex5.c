fea3be74ff15e2ce740c60b070c86335ecce0898
Diego Teruya <diegokteruya@gmail.com>
2025-03-19 22:35:55 +0000

    fix ex 5

--- mack/13/ex5.c
+++ mack/13/ex5.c
@@ -5,31 +5,22 @@
 
 int contarUnicos(int arr[], int n)
 {
-    int amount = 0;
-    int flag = 0;
-    int i = 0;
-    int j;
+    int count = 0;
+    int i, j;
+    int repetido;
 
-    while(i < n)
+    for (i = 0; i < n; i++)
     {
-        j = 0;
-        while (j < n)
+        repetido = 0;
+        for (j = 0; j < n; j++)
         {
-            if ((arr[i] == arr[j]) && (i != j))
-            {
-                amount++;
-                j++;
-            }
-            else
-                j++;
+            if (arr[i] == arr[j] && i != j)
+                repetido = 1;
         }
-        i++;
+        if (repetido == 0)
+            count++;
     }
-    amount = amount / 2;
-    if (amount % 2 != 0)
-        amount++;
-    amount = amount/2;
-    return (amount);
+    return count;
 }
 
 void ordena(int v[], int n)
@@ -63,7 +54,7 @@ int main(void)
     int v[10];
     geraVetorAleatorioOrdenadoCrescente(v, 10, 0, 10);
     imprimeVetor(v, 10);
-    printf("\n\n\n\n%d", contarUnicos(v, 10));
+    printf("\n\n\n\n%d\n", contarUnicos(v, 10));
 
     return (0);
 }
